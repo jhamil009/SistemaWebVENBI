1bd55d7ed5dc6795f6954a133991c6a2731b122a
Jhamil Guardia <jhamilguardia@gmail.com>
2022-10-17 15:52:59 -0400

    Modificacion del archivo modelo_model.php

--- application/models/modelo_model.php
+++ application/models/modelo_model.php
@@ -3,13 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Modelo_model extends CI_Model {
 
-	public function listar()
-	{
-		$this->db->select('*');
-		$this->db->from('modelo');
-		$this->db->where('estado', 1);
-		return $this->db->get();
-	}	
+	
 	public function listaDesabilitados()
 	{
 		$this->db->select('*');
